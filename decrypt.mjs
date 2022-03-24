5149b5974243ce5de34ee9be110a85e1b23be089
dtinth on MBP M1 <dtinth@spacet.me>
2022-03-24 12:01:59 +0700

    2 files changed, 88 insertions(+), 55 deletions(-)

--- decrypt.mjs
+++ decrypt.mjs
@@ -2,36 +2,51 @@ import nacl from './vendor/nacl.js'
 import fs from 'fs'
 import path from 'path'
 
-const chars = new Map(
-  [
-    '◰',
-    '◱',
-    '◲',
-    '◳',
-    '◴',
-    '◵',
-    '◶',
-    '◷',
-    '◸',
-    '◹',
-    '◺',
-    '◿',
-    '◢',
-    '◣',
-    '◤',
-    '◥',
-  ].map((c, i) => [c, i.toString(16)]),
-)
+const decode = (ciphertext) => {
+  let m
+  if ((m = ciphertext.match(/◔([^]*)◕/))) {
+    const chars = new Map(
+      [
+        '◰',
+        '◱',
+        '◲',
+        '◳',
+        '◴',
+        '◵',
+        '◶',
+        '◷',
+        '◸',
+        '◹',
+        '◺',
+        '◿',
+        '◢',
+        '◣',
+        '◤',
+        '◥',
+      ].map((c, i) => [c, i.toString(16)]),
+    )
+    return Buffer.from([...m[1]].map((x) => chars.get(x) || '').join(''), 'hex')
+  }
+  if ((m = ciphertext.match(/⡇([^]*)⢸/))) {
+    return Buffer.from(
+      [...m[1]].flatMap((x) => {
+        const charCode = x.charCodeAt(0)
+        return charCode >= 0x2800 && charCode <= 0x28ff
+          ? [charCode - 0x2800]
+          : []
+      }),
+    )
+  }
+}
+
 const bufs = []
 for await (const chunk of process.stdin) {
   bufs.push(chunk)
 }
 
 const ciphertext = Buffer.concat(bufs).toString('utf8')
-const buffer = Buffer.from(
-  [...ciphertext].map((x) => chars.get(x) || '').join(''),
-  'hex',
-)
+const buffer = decode(ciphertext)
+
 const sk = Buffer.from(
   fs.readFileSync(path.join(process.env.HOME, '.todecryptkey'), 'utf8'),
   'base64',
